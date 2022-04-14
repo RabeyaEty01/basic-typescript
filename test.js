f7b5237457adcb570e887bac577f7c8cbd7c8977
RabeyaEty01 <etyrabeya01@gmail.com>
2022-04-14 13:57:38 +0600

    commands added

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
+/* 
+//commands
+1. npm init -y
+2. npm install -g typescript
+3. npm install --save-dev typescript
+4. npx tsc fileName.ts
+5. npx tsc fileName.ts --watch
 
-function getLowerCase(str){
-    return str.toLowerCase();
-}
+6. node fileName.js
+7. npx tsc --init
+*/
 
-console.log(getLowerCase("HELLO"))
