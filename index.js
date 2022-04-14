9604d4ee723eea67c42bfab768030075e7ea468d
RabeyaEty01 <etyrabeya01@gmail.com>
2022-04-14 13:53:57 +0600

    some other functions added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var a = "Hello world";
-// string,number,Array,null, undefined,bollen
+// string,number,Array,null, undefined,boolean
 //any
 function getLowerCase(str) {
     return str.toLowerCase();
@@ -15,3 +15,26 @@ var substract = function (number1, number2) {
     return number1 - number2;
 };
 console.log(substract(2, 5));
+//Array<T>
+var arr = [123, 345, 2789];
+var arr2 = ["s", "t", "n"];
+var arr1 = ["123", "345", 2789];
+var id = "23";
+var person1 = {
+    name: "Noman",
+    age: 24,
+    hobby: "Sleeping"
+};
+var person2 = {
+    name: "Ety",
+    age: 22
+};
+//Enums
+var PI = 3.1416;
+var Colors;
+(function (Colors) {
+    Colors["color1"] = "#ededed";
+    Colors["color2"] = "#ededff";
+    Colors["color3"] = "#ededee";
+})(Colors || (Colors = {}));
+console.log(Colors.color1);
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const a: string = "Hello world";
 
-// string,number,Array,null, undefined,bollen
+// string,number,Array,null, undefined,boolean
 //any
 
 function getLowerCase(str: string) {
@@ -21,4 +21,55 @@ const substract = (number1: number, number2: number): number => {
     return number1 - number2
 }
 
-console.log(substract(2,5));
+console.log(substract(2, 5));
+
+//Array<T>
+const arr3: Array<number | string> = [123, 234, "Tamim"];
+const arr: number[] = [123, 345, 2789];
+const arr2: string[] = ["s", "t", "n"];
+const arr1: (string | number)[] = ["123", "345", 2789];
+
+type numberAndString = number | string;
+const id: numberAndString = "23";
+
+interface Iperson {
+    name: string;
+    age: number;
+    hobby?: string;
+}
+
+
+
+const person1: Iperson = {
+    name: "Noman",
+    age: 24,
+    hobby: "Sleeping",
+}
+
+const person2: Iperson = {
+    name: "Ety",
+    age: 22
+};
+
+const personArr:Array<Iperson> = [
+    person1, 
+    person2
+]
+//Enums
+
+const PI = 3.1416;
+
+enum Colors {
+    color1 = "#ededed",
+    color2 = "#ededff",
+    color3 = "#ededee",
+}
+
+console.log(Colors.color1);
+
+// function _concat<T>(a: T, b: T): T {
+//     return a + b
+// }
+
+// _concat<number>(1, 3)
+// _concat<String>("Tamim", "Noman");
